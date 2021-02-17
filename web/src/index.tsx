5c1eee4723496bdb31ad8a01de922be1507be055
Lucas Ferreira <lsferreirapel@gmail.com>
2021-02-17 20:32:57 -0300

    :wrench: Add API service
    - Add API service, send requests to backend
    - Add "dotenv" dependence
    - Create environment configuration

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as dotenv from 'dotenv';
+
 import App from './App';
+
+dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
